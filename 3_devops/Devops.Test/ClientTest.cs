e8aceb723304c081d7f3eea9aca894dea35495d6
benjaminclegg <b.clegg@live.com>
2019-12-10 10:01:50 -0600

    Added testing stage and coverlet to Devops solution

---
+++ 3_devops/Devops.Test/ClientTest.cs
@@ -0,0 +1,36 @@
+using Devops.Client.Controllers;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Devops.Test
+{
+   public class ClientTest
+   {
+      private readonly ILogger<HomeController> logger = LoggerFactory.Create(o => o.SetMinimumLevel(LogLevel.Debug)).CreateLogger<HomeController>();
+      [Fact]
+      public void Test_IndexPage()
+      {
+         // arrange
+         var home = new HomeController(logger);
+
+         // act
+         var view = home.Index();
+
+         // assert
+         Assert.NotNull(view);
+      }
+
+      [Fact]
+      public void Test_PrivacyPage()
+      {
+         // arrange
+         var home = new HomeController(logger);
+
+         // act
+         var view = home.Privacy();
+
+         // assert
+         Assert.NotNull(view);
+      }
+   }
+}
